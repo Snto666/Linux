314bdfc765acf835b58a47799c59ea3cc2249e67
Snto666 <edyambayfa@uide.edu.ec>
2025-12-03 15:45:16 +0000

    Script2 Dt Sy

--- script.sh/script2.sh
+++ script.sh/script2.sh
@@ -1,4 +1,4 @@
-uname -a #+
-lscpu #
-free -h #
-dr -h #
+uname -a # uname -a, obtiene informacion del sistema
+lscpu # lscpu, informacion detallada sobre la arquitectura de la cpu
+free -h # free -h, muestra la cantidad total de memoria fisica y de intercambio en el sistema
+df -h # df -h, muestra el uso del disco
