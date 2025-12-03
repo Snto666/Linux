693a30a42e21addfc287e472836b92eb21c5a3b4
Snto666 <edyambayfa@uide.edu.ec>
2025-12-03 15:41:43 +0000

    Primer script DavidT y SantiagoY

---
+++ script.sh/script.sh
@@ -0,0 +1,6 @@
+#Crear y listar archivos
+echo "Hola Linux" > archivo.txt #El comando echo se utiliza para enseñar texto en la pantalla
+cat archivo.txt # cat, se utiliza para mostrar el contenido de un archivo
+cp archivo.txt copia.txt #cp, se utiliza para copiar archivos de una ubicacion a otra
+ls -l # ls, se utiliza para listar aarchivos en un formato legible
+rm archivo.txt # rm, se utiliza para eliminar archivos directamente desde la terminal
